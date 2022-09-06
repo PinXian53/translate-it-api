dea12e032d5b96374fd5e683d7db27e5e0b26e81
Pin <pino.chang@tpisoftware.com>
2022-09-06 09:49:07 +0800

    feat: init project

---
+++ src/test/java/com/pino/translateitapi/TranslateItApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pino.translateitapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslateItApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
