66746f15c2e8530dcad27485f34dddb93fee022a
Pin <pino.chang@tpisoftware.com>
2022-09-09 00:48:02 +0800

    feat: 設定 max-query-complexity、max-query-depth

---
+++ src/main/java/com/pino/translateitapi/config/GraphQLConfig.java
@@ -0,0 +1,28 @@
+package com.pino.translateitapi.config;
+
+import graphql.analysis.MaxQueryComplexityInstrumentation;
+import graphql.analysis.MaxQueryDepthInstrumentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GraphQLConfig {
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "spring.graphql.instrumentation", name = "max-query-complexity")
+    public MaxQueryComplexityInstrumentation maxQueryComplexityInstrumentation(
+        @Value("${spring.graphql.instrumentation.max-query-complexity}") int maxComplexity) {
+        return new MaxQueryComplexityInstrumentation(maxComplexity);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @ConditionalOnProperty(prefix = "spring.graphql.instrumentation", name = "max-query-depth")
+    public MaxQueryDepthInstrumentation maxQueryDepthInstrumentation(
+        @Value("${spring.graphql.instrumentation.max-query-depth}") int maxDepth) {
+        return new MaxQueryDepthInstrumentation(maxDepth);
+    }
+}
